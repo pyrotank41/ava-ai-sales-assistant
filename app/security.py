0db8f5e00016a9bc9759b544906d6ff70e85d70b
Karan Singh Kochar <pyrotank41@gmail.com>
2024-08-04 15:10:09 -0500

    Dev (#21)
    * chore: Configure liveness probe for Azure container app
    
    * chore: Update Docker actions versions and Azure Container Registry login action
    
    * chore: Remove liveness probe configuration for Azure container app
    
    * updated cd
    
    * updated cd
    
    * bug fix
    
    * chore: Remove unnecessary code for loading environment variables
    
    * chore: Refactor code to ensure API keys are loaded from environment variables
    
    * chore: Refactor code to load API keys from environment variables
    
    * .
    
    * chore: Refactor code to ensure API keys are loaded from environment variables
    
    * Refactor code to ensure API keys are loaded from environment variables
    
    * chore: Refactor code to ensure API keys are loaded from environment variables

--- app/security.py
+++ app/security.py
@@ -2,24 +2,30 @@ from fastapi import HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
 from starlette.status import HTTP_403_FORBIDDEN
 import os
-
 from utils.load_env import load_env_vars
 
+
+
 def load_api_keys():
-    #check if the api keys are set in the environment
+    
+    # Check if the API keys are set in the environment
     key1 = os.getenv("API_KEY1")
     key2 = os.getenv("API_KEY2")
 
     if not key1 or not key2:
-        # load from remote storage
+        # Load from remote storage
         load_env_vars()
-        key1, key2 = load_api_keys()
 
-    # final validation
+        # Check again after loading from remote storage
+        key1 = os.getenv("API_KEY1")
+        key2 = os.getenv("API_KEY2")
+
+    # Final validation
     if not key1 or not key2:
         raise ValueError("API_KEY1 and API_KEY2 must be set in the environment")
 
     return key1, key2
+
 
 API_KEY1, API_KEY2 = load_api_keys()
 API_KEY_NAME = "access_token"
