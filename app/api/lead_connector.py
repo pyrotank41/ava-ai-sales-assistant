837cc3bb5278b6761a3d346c88e67ab532f89aa1
Karan Singh Kochar <pyrotank41@gmail.com>
2024-08-15 19:12:20 -0600

    Feature/enhance/lc message service (#54)
    * refactor: Update config with new conversation count limit
    
    * refactor: Update config with new conversation count limit
    
    * refactor: Add AVA-related tags and custom fields
    
    ```
    
    * refactor: Add AGENT_ENGAGED_TAG to LeadConnector messaging service
    
    * refactor: Fix condition for checking if contact is engaged by an agent
    
    * refactor: Update message type mapping in leadconnector.py
    
    * refactor: Update engage_contact endpoint as well as leadconnector webhook to sync def instead of async

--- app/api/lead_connector.py
+++ app/api/lead_connector.py
@@ -8,21 +8,21 @@ from services.lead_connector_messaging_service import LeadConnectorMessageingSer
 
 router = APIRouter()
 
-@router.get("/engage_contacts")
-async def engage_contact(contact_id, location_id, message_type:LCMessageType = LCMessageType.TYPE_SMS):
+@router.get("/engage_contact")
+def engage_contact(contact_id, location_id, message_type:LCMessageType = LCMessageType.TYPE_SMS):
     return LeadConnectorMessageingService(location_id=location_id).engage_with_contact(contact_id=contact_id, message_type=message_type)
 
 @router.get("/contact")
-async def get_contact():
+def get_contact():
     return LeadConnector(location_id="hqDwtNvswsupf6BT1Qxt").get_contact_info(
         contact_id="6ygr3QkYGJfEGfNJ2FvD"
     )
 
 @router.get("/custom_fields")
-async def get_custom_fields(location_id):
+def get_custom_fields(location_id):
     return LeadConnector(location_id=location_id).get_custom_fields()
     
 @router.get("/custom_fields/id_key_mapping")
-async def get_custom_fields_id_key_mapping(location_id):
+def get_custom_fields_id_key_mapping(location_id):
     return LeadConnector(location_id=location_id).get_custom_fields_id_key_mapping()
 
